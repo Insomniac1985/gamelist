e24fc3728e4a78b402ed4bbec426b6543b55a3e6
ShabiiEXE <ShabiiEXE@users.noreply.github.com>
2026-06-12 19:41:33 +0200

    Align trophy sort controls

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "gamelist-cache-v37";
+const CACHE_VERSION = "gamelist-cache-v38";
 const STATIC_CACHE = `${CACHE_VERSION}:static`;
 const MEDIA_CACHE = `${CACHE_VERSION}:media`;
 const STATIC_ASSETS = [
