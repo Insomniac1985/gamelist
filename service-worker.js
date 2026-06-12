9bf4efa435e05154d8ae358fa450b9805db3e4fa
ShabiiEXE <ShabiiEXE@users.noreply.github.com>
2026-06-12 19:36:21 +0200

    Rework trailer background blend

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "gamelist-cache-v35";
+const CACHE_VERSION = "gamelist-cache-v36";
 const STATIC_CACHE = `${CACHE_VERSION}:static`;
 const MEDIA_CACHE = `${CACHE_VERSION}:media`;
 const STATIC_ASSETS = [
