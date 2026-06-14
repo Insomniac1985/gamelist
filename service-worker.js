186aeb46b9bed87e6955c60fed297647bc0cf1ad
ShabiiEXE <ShabiiEXE@users.noreply.github.com>
2026-06-14 19:08:09 +0200

    Style stream edit toggle

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "gamelist-cache-v56";
+const CACHE_VERSION = "gamelist-cache-v57";
 const STATIC_CACHE = `${CACHE_VERSION}:static`;
 const MEDIA_CACHE = `${CACHE_VERSION}:media`;
 const STATIC_ASSETS = [
