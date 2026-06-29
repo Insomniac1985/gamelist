d1af2ac2e30a930358066520bcbbeed17edcad01
ShabiiEXE <ShabiiEXE@users.noreply.github.com>
2026-06-29 17:15:29 +0200

    Use accent titles on shelf modules

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "gamelist-cache-v261";
+const CACHE_VERSION = "gamelist-cache-v262";
 const STATIC_CACHE = `${CACHE_VERSION}:static`;
 const MEDIA_CACHE = `${CACHE_VERSION}:media`;
 const STATIC_ASSETS = [
