100422d97c2f8b6232b4c684f9faab2801bf3659
ShabiiEXE <ShabiiEXE@users.noreply.github.com>
2026-07-03 10:02:24 +0200

    Widen platform dropdowns

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "gamelist-cache-v301";
+const CACHE_VERSION = "gamelist-cache-v302";
 const STATIC_CACHE = `${CACHE_VERSION}:static`;
 const MEDIA_CACHE = `${CACHE_VERSION}:media`;
 const STATIC_ASSETS = [
