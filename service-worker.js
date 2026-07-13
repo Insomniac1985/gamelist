afdc608bb7528caad5284106914d6a1b1d9a6ab0
ShabiiEXE <ShabiiEXE@users.noreply.github.com>
2026-07-13 22:39:08 +0200

    Add shelf settings close hover state

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "gamelist-cache-v368";
+const CACHE_VERSION = "gamelist-cache-v369";
 const STATIC_CACHE = `${CACHE_VERSION}:static`;
 const MEDIA_CACHE = `${CACHE_VERSION}:media`;
 const STATIC_ASSETS = [
