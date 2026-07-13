4b495860d18fe54c16699914d434ce8477aa5b83
ShabiiEXE <ShabiiEXE@users.noreply.github.com>
2026-07-13 22:28:40 +0200

    Align finished year dropdown label

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "gamelist-cache-v363";
+const CACHE_VERSION = "gamelist-cache-v364";
 const STATIC_CACHE = `${CACHE_VERSION}:static`;
 const MEDIA_CACHE = `${CACHE_VERSION}:media`;
 const STATIC_ASSETS = [
