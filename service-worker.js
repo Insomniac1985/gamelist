4aade355984a4394a6e5653b31af7ee3bfbea948
ShabiiEXE <ShabiiEXE@users.noreply.github.com>
2026-07-13 23:35:04 +0200

    Adjust GOTY card label styling

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "gamelist-cache-v383";
+const CACHE_VERSION = "gamelist-cache-v384";
 const STATIC_CACHE = `${CACHE_VERSION}:static`;
 const MEDIA_CACHE = `${CACHE_VERSION}:media`;
 const STATIC_ASSETS = [
