6280f0e1dfc5a3005d6bc8fb2e4fe296582b3b5a
ShabiiEXE <ShabiiEXE@users.noreply.github.com>
2026-07-14 10:14:20 +0200

    Refine GOTY carousel masks

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "gamelist-cache-v391";
+const CACHE_VERSION = "gamelist-cache-v392";
 const STATIC_CACHE = `${CACHE_VERSION}:static`;
 const MEDIA_CACHE = `${CACHE_VERSION}:media`;
 const STATIC_ASSETS = [
