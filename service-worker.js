9674573d828ace6fa564479a8012f56e550190f9
ShabiiEXE <ShabiiEXE@users.noreply.github.com>
2026-07-14 16:32:39 +0200

    Flatten GOTY card backgrounds

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "gamelist-cache-v405";
+const CACHE_VERSION = "gamelist-cache-v406";
 const STATIC_CACHE = `${CACHE_VERSION}:static`;
 const MEDIA_CACHE = `${CACHE_VERSION}:media`;
 const STATIC_ASSETS = [
