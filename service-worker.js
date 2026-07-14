d886ca91b9c74cd1a08a2cbeae30b98f2a22c181
ShabiiEXE <ShabiiEXE@users.noreply.github.com>
2026-07-14 23:38:25 +0200

    Soften floating add hover

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "gamelist-cache-v434";
+const CACHE_VERSION = "gamelist-cache-v435";
 const STATIC_CACHE = `${CACHE_VERSION}:static`;
 const MEDIA_CACHE = `${CACHE_VERSION}:media`;
 const STATIC_ASSETS = [
