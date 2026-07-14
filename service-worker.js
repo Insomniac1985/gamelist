3b97c49ac8f00733626f1913e762f7de06c59e7e
ShabiiEXE <ShabiiEXE@users.noreply.github.com>
2026-07-14 16:33:41 +0200

    Tighten GOTY header spacing

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "gamelist-cache-v406";
+const CACHE_VERSION = "gamelist-cache-v407";
 const STATIC_CACHE = `${CACHE_VERSION}:static`;
 const MEDIA_CACHE = `${CACHE_VERSION}:media`;
 const STATIC_ASSETS = [
