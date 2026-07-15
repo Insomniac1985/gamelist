ee571c180dac4e6154124e0234c981a44b127879
ShabiiEXE <ShabiiEXE@users.noreply.github.com>
2026-07-15 19:23:37 +0200

    Center mobile stats popovers

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "gamelist-cache-v501";
+const CACHE_VERSION = "gamelist-cache-v502";
 const STATIC_CACHE = `${CACHE_VERSION}:static`;
 const MEDIA_CACHE = `${CACHE_VERSION}:media`;
 const STATIC_ASSETS = [
