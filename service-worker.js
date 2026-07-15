f0f268d235ead9fdd9b92083a94af35840950248
ShabiiEXE <ShabiiEXE@users.noreply.github.com>
2026-07-15 09:52:29 +0200

    Arrange release stats KPI row

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "gamelist-cache-v478";
+const CACHE_VERSION = "gamelist-cache-v479";
 const STATIC_CACHE = `${CACHE_VERSION}:static`;
 const MEDIA_CACHE = `${CACHE_VERSION}:media`;
 const STATIC_ASSETS = [
