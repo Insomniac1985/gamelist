7da05f4a8ba2c34021705ca18588db61a9419482
ShabiiEXE <ShabiiEXE@users.noreply.github.com>
2026-07-16 01:11:15 +0200

    Restore GOTY export platform label radius

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "gamelist-cache-v559";
+const CACHE_VERSION = "gamelist-cache-v560";
 const STATIC_CACHE = `${CACHE_VERSION}:static`;
 const MEDIA_CACHE = `${CACHE_VERSION}:media`;
 const STATIC_ASSETS = [
