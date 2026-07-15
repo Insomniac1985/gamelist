8c2458216296c70544bfd3c05eaadcbfbae50079
ShabiiEXE <ShabiiEXE@users.noreply.github.com>
2026-07-15 19:33:50 +0200

    Force completed stats hover gold

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "gamelist-cache-v505";
+const CACHE_VERSION = "gamelist-cache-v506";
 const STATIC_CACHE = `${CACHE_VERSION}:static`;
 const MEDIA_CACHE = `${CACHE_VERSION}:media`;
 const STATIC_ASSETS = [
